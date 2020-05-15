424906d5054f232250a6d72e4f1cf9073227959c
Marius Murschel <mm@rgweb.de>
2020-05-15 21:59:27 +0200

    gui is able to load files and show thumbnails

--- WeatherBackgroundChanger.py
+++ WeatherBackgroundChanger.py
@@ -7,7 +7,7 @@ import schedule
 
 import pystray
 from pystray import MenuItem as item
-from PIL import Image, ImageDraw
+from PIL import Image, ImageTk, ImageDraw
 import os
 from dotenv import load_dotenv
 
@@ -23,6 +23,9 @@ API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
 CITY = os.getenv("HOME_TOWN")
 REFRESH_SECONDS = int(os.getenv("BACKGROUND_REFRESH_SECONDS"))
 SPI_SETDESKWALLPAPER = 20
+
+app = None
+guiThread = None
 
 api_address='http://api.openweathermap.org/data/2.5/weather?appid='
 
@@ -118,9 +121,11 @@ def exitProgram():
     print("Exiting")
     schedule.clear()
     icon.stop()
+    destroyGui()
 
 # GUI ---------------------------------------------------
 import tkinter as tk
+from tkinter import filedialog
 import threading
 
 class Application(tk.Frame):
@@ -128,39 +133,103 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         # Dark gray: #151819
+        self.wallpaperDir = PICTURE_PATH
 
         self.master.title("WeatherDesk settings")
         self.master.iconphoto(False, tk.PhotoImage(file='WeatherDeskIcon.png'))
         # self.master.resizable(width=False, height=False)
-        self.master.geometry('350x200')
+        self.master.geometry('600x400')
         self.master['bg'] = "#151819"
         self.master.bind("<Escape>", lambda e: e.widget.quit())
         
         self.create_widgets()
-        self.pack()
+        self.generateWallpaperThumbnails()
 
-        # windowWidth = self.master.winfo_reqwidth()
-        # windowHeight = self.master.winfo_reqheight()
-        # print("Width",windowWidth,"Height",windowHeight)
-        # Gets both half the screen width/height and window width/height
-        # positionRight = int(self.master.winfo_screenwidth()/2 - windowWidth/2)
-        # positionDown = int(self.master.winfo_screenheight()/2 - windowHeight/2)
-        # self.master.geometry('+{}+{}'.format(positionRight, positionDown))
+        # self.grid()
+        self.pack()
 
 
     def create_widgets(self):
-        self.quit = tk.Button(self, text="QUIT", bg="white", fg="red",
+        self.quit = tk.Button(self, text="close", bg="white", fg="red",
                               command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.grid(row=0, column=1)
+        # self.quit.pack()
+
+        self.openWallpaperDir = tk.Button(self, text="Choose Wallpaper Folder", bg="white", fg="black",
+                              command=self.chooseWallpaperDir)
+        self.openWallpaperDir.grid(row=0, column=0)
+        # self.openWallpaperDir.pack()
+
+    def clearWallpaperCheckboxesAndThumbnails(self):
+        self.wallpaperThumbnails = []
+        if len(self.wallpaperLabels) > 0:
+            for label in self.wallpaperLabels:
+                label.destroy()
+        if len(self.checkboxes) > 0:
+            for check in self.checkboxes:
+                check.destroy()
+
+    def generateWallpaperThumbnails(self):
+        if self.wallpaperDir is not None:
+            self.wallpaperFiles = [f for f in os.listdir(self.wallpaperDir) if os.path.isfile(os.path.join(self.wallpaperDir, f)) and not f.endswith(".thumbnail") and f.find("Thumbnail") == -1]
+            print("Found these Wallpapers:")
+            self.checkboxes = []
+            self.wallpaperThumbnails = []
+            self.wallpaperLabels = []
+            for (index, filename) in enumerate(self.wallpaperFiles):
+                print(filename)
+                filepath = os.path.join(self.wallpaperDir, filename)
+                # Create thumbnail if none exists
+                suffix = filename.split('.')[1]
+                outfilename = filename.split('.')[0] + "Thumbnail." + suffix
+                outfilepath = os.path.join(self.wallpaperDir, outfilename)
+                thumbnailExists = True
+                try:
+                    Image.open(outfilepath)
+                except FileNotFoundError:
+                    thumbnailExists = False
+                if not thumbnailExists:
+                    print("Creating thumbnail for", filename)
+                    try:
+                        thumbnail = Image.open(filepath)
+                        thumbnail.thumbnail((80, 80), Image.NEAREST)
+                        thumbnail.save(outfilepath)
+                    except:
+                        print("could not create thumbnail for",filename)
+                thumb = ImageTk.PhotoImage(Image.open(outfilepath))
+                wallpaperLabel = tk.Label(self, image=thumb)
+                wallpaperLabel.grid(row=index+1, column=1)
+                # wallpaperLabel.pack(side = "bottom", fill = "both", expand = "yes")
+                self.wallpaperLabels.append(wallpaperLabel)
+                self.wallpaperThumbnails.append(thumb)
+                checkbox = tk.Checkbutton(self, text=filename.split('.')[0])
+                checkbox.grid(row=index+1, column=0)
+                # checkbox.pack()
+                self.checkboxes.append(checkbox)
+
+    def chooseWallpaperDir(self):
+        self.wallpaperDir = filedialog.askdirectory(title="Choose the folder which contain your Wallpapers",initialdir=os.path.join(USER_PATH,'Pictures'))
+        print("Your Wallpaper directory now is:",self.wallpaperDir)
+        PICTURE_PATH = self.wallpaperDir
+        self.clearWallpaperCheckboxesAndThumbnails()
+        self.generateWallpaperThumbnails()
+            
+
+def destroyGui():
+    if guiThread is not None:
+        app.destroy()
+        guiThread.join()
 
 def tkinterGui():
+    global app
     root = tk.Tk()
     app = Application(master=root)
     root.mainloop()
 
-def startGuiThread():    
-    GUI = threading.Thread(target=tkinterGui)
-    GUI.start()
+def startGuiThread():
+    global guiThread    
+    guiThread = threading.Thread(target=tkinterGui)
+    guiThread.start()
     # GUI.join()
 
 # GUI END ---------------------------------------------------
